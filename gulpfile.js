d66362cfaffa012f9d1565a9edf2eeff04971b05
Edvaldo da Rosa <fraterblack@bck.com.br>
2017-07-21 16:02:24 -0300

    Limpeza gulpfile e dependências

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-// Tasks RequireJs
-var gulp = require('gulp'),
-    requireDir = require('require-dir');
-
-//Tasks do Site
-//requireDir('./gulp', { recurse: true });
-
-//Tasks do Painel
-requireDir('./gulp-panel', { recurse: true });
